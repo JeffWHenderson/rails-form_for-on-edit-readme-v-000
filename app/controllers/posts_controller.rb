828d807d81162f28e0bbf4c24ca66d18957db936
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-19 05:03:06 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,10 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  # @post = Post.find(params[:id])
+	  @post = Post.find(params[:id])
 	  # @post.update(title: params[:title], description: params[:description])
-	  # redirect_to post_path(@post)
 		@post.update(params.require(:post))
+	  redirect_to post_path(@post)
+
 	end
 end
